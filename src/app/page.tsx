562b396234f4a8f11507a15072fdff6db49b67fe
Ajay Kumar <ajaynew83@gmail.com>
2024-12-30 18:32:41 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProfilePage from "./profile/page";
 
 interface Item {
@@ -9,12 +8,6 @@ interface Item {
 
 export default function Home() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/about" element={<ProfilePage /> } />
-        <Route path="/" element={<ProfilePage /> } />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
+    <ProfilePage />
   );
 }
