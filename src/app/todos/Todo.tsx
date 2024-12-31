551a45c9a29f923f5ee6890299e95ed062505ec5
Ajay Kumar <ajaynew83@gmail.com>
2024-12-31 12:36:28 +0530

    added handlecheck function

--- src/app/todos/Todo.tsx
+++ src/app/todos/Todo.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import {Todo} from '@/app/_types/todos/Todo'
 
-const TodoItem: React.FC<Todo> = (todo) => {
+type TodoItemProps = {
+  todo: Todo, 
+  handleCheck: (id: number) => void
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo, handleCheck }) => {
   return <>
     <li key={todo.id} className='font-light flex justify-between border-2 m-[5px] p-2'>
         <span>{todo.title}</span>
-        <input type="checkbox" checked={todo.completed} readOnly />
+        <input type="checkbox" checked={todo.completed} onChange={() => handleCheck(todo.id)}/>
     </li>
   </>
 }
--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -6,6 +6,10 @@ import { Todo } from '@/app/_types/todos/Todo';
 const TodosPage = () => {
     const {data, loading, error} = useFetchHook<Todo[]>('https://jsonplaceholder.typicode.com/todos');
     
+    const handlecheck = (id: number) => {
+        console.log("id value", id);
+    }
+
     return (
         <div className='mt-2 ml-5'>
             <h1 className='font-semibold'>Todos List</h1>
@@ -14,7 +18,7 @@ const TodosPage = () => {
             {data && data.length > 0 && 
             <ul className='w-1/2 flex flex-col'>
                 {data?.map(todo => (
-                    <TodoItem {...todo} key={todo.id}/>
+                    <TodoItem todo={todo} key={todo.id} handleCheck={handlecheck}/>
                 ))}
             </ul>
             }
